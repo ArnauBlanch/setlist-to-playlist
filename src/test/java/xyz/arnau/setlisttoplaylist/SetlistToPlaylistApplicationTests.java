84cc57af85114ed494d6691513490ff8e532f0fe
Arnau Blanch <arnaubc@gmail.com>
2022-12-02 20:19:45 +0100

    Initial commit

---
+++ src/test/java/xyz/arnau/setlisttoplaylist/SetlistToPlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.arnau.setlisttoplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetlistToPlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
