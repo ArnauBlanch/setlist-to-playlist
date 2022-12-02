50d9e1230e4533f5ff7cb7c70943f124eb8a0fb3
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2022-12-02 20:19:45 +0100

    Initial commit

---
+++ src/main/java/xyz/arnau/setlisttoplaylist/SetlistToPlaylistApplication.java
@@ -0,0 +1,13 @@
+package xyz.arnau.setlisttoplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetlistToPlaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetlistToPlaylistApplication.class, args);
+	}
+
+}
