d3f6d3f7c3ff5b80a7aaee63755e8811358e8920
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2022-12-03 18:06:32 +0100

    Change Date to LocalDate

--- src/main/java/xyz/arnau/setlisttoplaylist/domain/Setlist.java
+++ src/main/java/xyz/arnau/setlisttoplaylist/domain/Setlist.java
@@ -2,8 +2,8 @@ package xyz.arnau.setlisttoplaylist.domain;
 
 import lombok.Builder;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 @Builder
-public record Setlist(Date date, Artist artist, Venue venue, List<Song> songs) {}
+public record Setlist(LocalDate date, Artist artist, Venue venue, List<Song> songs) {}
