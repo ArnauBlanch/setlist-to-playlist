037fd6a5be106ba7faee70fd562a3243a9b50dc5
Arnau Blanch <arnaubc@gmail.com>
2022-12-16 13:21:40 +0100

    Add Tailwind Prettier plugin

---
+++ frontend/app/NavBar.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+
+export default function NavBar() {
+	return (
+		<nav className="sticky top-0 left-0 z-20 h-12 w-full bg-white px-4 py-2.5 dark:bg-gray-900 sm:px-4 md:h-16">
+			<div className="container mx-auto flex flex-wrap items-center justify-between">
+				<Link href="/" className="flex items-center">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						version="1.1"
+						x="0px"
+						y="0px"
+						viewBox="0 0 60 60"
+						xmlSpace="preserve"
+						className="mr-3 h-8 w-8 bg-gradient-to-r from-amber-400 to-rose-700 fill-white dark:fill-gray-900"
+					>
+						<path
+							id="path180"
+							d="M -0.234375,-0.234375 V 60.058594 H 60.466797 V -0.234375 Z M 30,0 C 46.541983,0 60,13.458017 60,30 60,46.541983 46.541983,60 30,60 13.458017,60 0,46.541983 0,30 0,13.458017 13.458017,0 30,0 Z m -6.943359,14.001953 c -0.178375,-0.01025 -0.359438,0.02628 -0.523438,0.113281 C 22.205203,14.288234 22,14.629 22,15 v 30 c 0,0.371 0.205203,0.711766 0.533203,0.884766 C 22.679203,45.962766 22.84,46 23,46 c 0.197,0 0.3935,-0.05883 0.5625,-0.173828 l 22,-15 C 45.8355,30.640172 46,30.331 46,30 46,29.669 45.836453,29.359828 45.564453,29.173828 l -22,-15 c -0.1535,-0.104 -0.329438,-0.161625 -0.507812,-0.171875 z"
+						/>
+					</svg>
+					<span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+						Setlist to Playlist
+					</span>
+				</Link>
+			</div>
+		</nav>
+	)
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,35 +1,35 @@
 export default function Home() {
 	return (
 		<div>
-			<div className="max-w-3xl mx-auto mt-4 lg:mt-4 text-center md:mx-auto mb-8 md:mb-20">
-				<h1 className="mb-2 lg:mb-4 text-4xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-7xl">
+			<div className="mx-auto mt-4 mb-8 max-w-3xl text-center md:mx-auto md:mb-20 lg:mt-4">
+				<h1 className="mb-2 text-4xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:mb-4 lg:text-7xl">
 					Create your new
 				</h1>
-				<h1 className="mb-6 md:mb-10 text-4xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-7xl">
-					<span className="text-transparent bg-clip-text bg-gradient-to-r from-amber-400 to-rose-600">
+				<h1 className="mb-6 text-4xl font-extrabold text-gray-900 dark:text-white md:mb-10 md:text-5xl lg:text-7xl">
+					<span className="bg-gradient-to-r from-amber-400 to-rose-600 bg-clip-text text-transparent">
 						favourite playlist
 					</span>
 					.
 				</h1>
-				<p className="mb-4 text-lg font-bold text-rose-600 md:text-xl lg:text-2xl dark:text-gray-400">
+				<p className="mb-4 text-lg font-bold text-rose-600 dark:text-gray-400 md:text-xl lg:text-2xl">
 					Create a Spotify playlist from a live concert setlist
 				</p>
-				<p className="mb-6 text-sm font-normal text-gray-500 md:text-md lg:text-lg dark:text-gray-500">
+				<p className="md:text-md mb-6 text-sm font-normal text-gray-500 dark:text-gray-500 lg:text-lg">
 					Get ready for the upcoming concerts of your favourite
 					artists, so you can practice your singing skills and enjoy
 					the experience to the fullest!
 				</p>
 			</div>
 
-			<form className="flex items-center max-w-3xl mx-auto">
+			<form className="mx-auto flex max-w-3xl items-center">
 				<label htmlFor="simple-search" className="sr-only">
 					Search artist
 				</label>
 				<div className="relative w-full">
-					<div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+					<div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
 						<svg
 							aria-hidden="true"
-							className="w-6 h-6 text-gray-500 dark:text-gray-400"
+							className="h-6 w-6 text-gray-500 dark:text-gray-400"
 							fill="currentColor"
 							viewBox="0 0 20 20"
 							xmlns="http://www.w3.org/2000/svg"
@@ -44,17 +44,17 @@ export default function Home() {
 					<input
 						type="text"
 						id="simple-search"
-						className="bg-gray-50 border border-gray-300 text-gray-900 text-md lg:text-xl rounded-lg focus:ring-rose-500 focus:border-rose-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-rose-700 dark:focus:border-rose-700"
+						className="text-md block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-gray-900 focus:border-rose-500 focus:ring-rose-500 dark:border-gray-600  dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-rose-700 dark:focus:ring-rose-700 lg:text-xl"
 						placeholder="Search artist"
 						required
 					/>
 				</div>
 				<button
 					type="submit"
-					className="p-2.5 ml-2 text-sm font-medium text-white bg-rose-600 rounded-lg border border-rose-600 hover:bg-rose-700 focus:ring-4 focus:outline-none focus:ring-rose-300 dark:bg-rose-700 dark:border-rose-700 dark:hover:bg-rose-800 dark:focus:ring-rose-800"
+					className="ml-2 rounded-lg border border-rose-600 bg-rose-600 p-2.5 text-sm font-medium text-white hover:bg-rose-700 focus:outline-none focus:ring-4 focus:ring-rose-300 dark:border-rose-700 dark:bg-rose-700 dark:hover:bg-rose-800 dark:focus:ring-rose-800"
 				>
 					<svg
-						className="w-6 h-6 lg:w-7 lg:h-7"
+						className="h-6 w-6 lg:h-7 lg:w-7"
 						fill="none"
 						stroke="currentColor"
 						viewBox="0 0 24 24"
@@ -70,14 +70,14 @@ export default function Home() {
 					<span className="sr-only">Search artist</span>
 				</button>
 			</form>
-			<div className="max-w-3xl mx-auto rounded-lg py-4 grid content-start grid-cols-1 md:grid-cols-2 gap-4">
-				<div className="group bg-gradient-to-r from-amber-200 to-rose-400 hover:bg-rose-100 hover:border-rose-200 rounded-3xl flex flex-row items-center p-4">
+			<div className="mx-auto grid max-w-3xl grid-cols-1 content-start gap-4 rounded-lg py-4 md:grid-cols-2">
+				<div className="group flex flex-row items-center rounded-3xl bg-gradient-to-r from-amber-200 to-rose-400 p-4 hover:border-rose-200 hover:bg-rose-100">
 					<img
-						className="group-hover:opacity-80 object-cover rounded-full aspect-square h-28"
+						className="aspect-square h-28 rounded-full object-cover group-hover:opacity-80"
 						alt="The Black Keys"
 						src="https://ca-times.brightspotcdn.com/dims4/default/3c74377/2147483647/strip/true/crop/6000x4000+0+0/resize/1200x800!/format/webp/quality/80/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F46%2Fcb%2F4583686798f1da737b90db957ab0%2Fc3a8c6131aa348d291c9e82450e45ddf"
 					/>
-					<div className="font-bold text-white group-hover:text-rose-700 text-sm md:text-2xl ml-4">
+					<div className="ml-4 text-sm font-bold text-white group-hover:text-rose-700 md:text-2xl">
 						The Black Keys
 					</div>
 				</div>
