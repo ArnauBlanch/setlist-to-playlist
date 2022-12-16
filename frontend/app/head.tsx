a0a818e7311e922c298e11230eb07c1991706f4d
Arnau Blanch <arnaubc@gmail.com>
2022-12-16 13:00:58 +0100

    Add Prettier + basic UI

--- frontend/app/head.tsx
+++ frontend/app/head.tsx
@@ -1,10 +1,16 @@
 export default function Head() {
-  return (
-    <>
-      <title>Setlist to Playlist</title>
-      <meta content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" name="viewport" />
-      <meta name="description" content="Create a Spotify playlist from a setlist" />
-      <link rel="icon" href="/favicon.ico" />
-    </>
-  )
+	return (
+		<>
+			<title>Setlist to Playlist</title>
+			<meta
+				content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
+				name="viewport"
+			/>
+			<meta
+				name="description"
+				content="Create a Spotify playlist from a setlist"
+			/>
+			<link rel="icon" href="/favicon.ico" />
+		</>
+	)
 }
